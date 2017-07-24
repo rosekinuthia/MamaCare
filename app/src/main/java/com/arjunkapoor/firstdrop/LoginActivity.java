8ecbe2bcbd3c6a3ae51df39fc5817338ddc14a1a
techtenant <bnyilima@gmail.com>
2017-07-24 12:19:38 +0300

    Logo

--- app/src/main/java/com/arjunkapoor/firstdrop/LoginActivity.java
+++ app/src/main/java/com/arjunkapoor/firstdrop/LoginActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+//UI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
